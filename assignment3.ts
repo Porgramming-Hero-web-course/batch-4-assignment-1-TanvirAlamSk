46ba26c7a8f1175a853fa02b44be3836da3c61b2
Tanvir Alam <86076771+TanvirAlamSk@users.noreply.github.com>
2024-12-16 21:24:57 +0600

    Delete assignment3.ts

--- assignment3.ts
+++
@@ -1,9 +0,0 @@
-// Solution 3
-const countWordOccurrences = (sentence: string, word: string): number => {
-  let count: number = 0;
-  const sentenceSplit: string[] = sentence.split(" ");
-  sentenceSplit.forEach(
-    (term) => term.toLowerCase() == word.toLowerCase() && count++
-  );
-  return count;
-};
