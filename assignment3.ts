24c9ec8532e29c0f13d9c4371404bea612a14c9c
Tanvir Alam <86076771+TanvirAlamSk@users.noreply.github.com>
2024-12-16 21:13:24 +0600

    Update assignment3.ts

--- assignment3.ts
+++ assignment3.ts
@@ -1,7 +1,9 @@
 // Solution 3
 const countWordOccurrences = (sentence: string, word: string): number => {
-    let count: number = 0;
-    const sentenceSplit: string[] = sentence.split(" ");
-    sentenceSplit.forEach((term) => term == word && count++);
-    return count;
-  };
+  let count: number = 0;
+  const sentenceSplit: string[] = sentence.split(" ");
+  sentenceSplit.forEach(
+    (term) => term.toLowerCase() == word.toLowerCase() && count++
+  );
+  return count;
+};
