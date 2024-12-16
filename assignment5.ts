7a90871fed3e9f22a89a30c5b745c201f9a54f21
Tanvir Alam <86076771+TanvirAlamSk@users.noreply.github.com>
2024-12-16 21:25:23 +0600

    Delete assignment5.ts

--- assignment5.ts
+++
@@ -1,9 +0,0 @@
-{
-  // Solution 5
-  const getProperty = <X, Y extends keyof X>(param: X, key: Y) => {
-    return param[key];
-  };
-
-  const person = { name: "Alice", age: 30 };
-  console.log(getProperty(person, "age")); //output : 30
-}
